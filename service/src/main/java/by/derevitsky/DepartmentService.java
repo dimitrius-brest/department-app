f196076843d95aa183427a9e7274dabc53d75eca
Dmitry Derevitsky <d_der@mail.ru>
2018-05-26 21:36:03 +0300

    DepartmentService class added

---
+++ service/src/main/java/by/derevitsky/DepartmentService.java
@@ -0,0 +1,22 @@
+package by.derevitsky;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentService {
+
+    public Department getById(Integer id) {
+        return null;
+    }
+
+    public void save(Department department) {
+    }
+
+    public void delete(Integer id) {
+    }
+
+    public List<Department> getAll() {
+        List<Department> departments = new ArrayList<Department>();
+        return departments;
+    }
+}
