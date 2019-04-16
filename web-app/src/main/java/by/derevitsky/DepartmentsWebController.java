248af72953d19ffc3cea0e75437ec2344f37b66c
Dmitry Derevitsky <d_der@mail.ru>
2019-04-16 21:46:01 +0300

    Web layer updated

--- web-app/src/main/java/by/derevitsky/DepartmentsWebController.java
+++ web-app/src/main/java/by/derevitsky/DepartmentsWebController.java
@@ -1,6 +1,6 @@
 package by.derevitsky;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -9,55 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping("/departments")
 public class DepartmentsWebController {
 
-    /**
-     * Gets Departments from REST API,
-     * adds "average salary" and "has employees" flag to every Department in list.
-     * @return the list of Departments + average salary + has employees flag
-     */
-    private List<DepartmentForView> getDepartments(){
-        List<DepartmentForView> departmentsForView = new ArrayList<DepartmentForView>();
-
-        // Getting array of Departments form REST API
-        RestTemplate restTemplate = new RestTemplate();
-        String url ="http://localhost:8080/department-rest/departments/all";
-        ResponseEntity<Department[]> responseEntity = restTemplate.getForEntity(url, Department[].class);
-        Department[] departments = responseEntity.getBody();
-
-        // Counting average salary for each Department in array
-        boolean hasEmployees;
-        int averageSalary;
-        for(Department dep : departments) {
-            String url2 = "http://localhost:8080/department-rest/employees/dep/" + dep.getId();
-            // Trying to get the list of Employees in the Department
-            try {
-                ResponseEntity<Employee[]> responseEntity2 = restTemplate.getForEntity(url2, Employee[].class);
-                Employee[] employees = responseEntity2.getBody();
-                hasEmployees = true;
-                int sum = 0;
-                // Counting average salary of Employees in the Department
-                for (int i = 0; i < employees.length; i++) {
-                    sum += employees[i].getSalary();
-                }
-                averageSalary = sum / employees.length;
-            } catch (Exception e) {
-                hasEmployees = false;
-                averageSalary = 0;
-            }
-            departmentsForView.add(new DepartmentForView(dep, averageSalary, hasEmployees));
-        }
-        return departmentsForView;
-    }
+    @Autowired
+    private DepartmentsWebService webService;
 
     /**
      * Show the list of Departments with average salaries
@@ -65,7 +26,7 @@ public class DepartmentsWebController {
      */
     @GetMapping("/all")
     public ModelAndView showDepartments(){
-        List<DepartmentForView> departments = this.getDepartments();
+        List<DepartmentForView> departments = webService.getDepartments(); //this.getDepartments();
 
         ModelAndView model = new ModelAndView("departments");
         model.addObject("departments", departments);
---
+++ web-app/src/main/java/by/derevitsky/DepartmentsWebService.java
@@ -0,0 +1,56 @@
+package by.derevitsky;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Service layer for Web module. Consumes REST API and redirects data to Web Conroller.
+ */
+@Service
+public class DepartmentsWebService {
+
+    private String applicationURL = "http://localhost:8080/department-rest";
+
+    /**
+     * Gets Departments from REST API,
+     * adds "average salary" and "has employees" flag to every Department in list.
+     * @return the list of Departments + average salary + has employees flag
+     */
+    public List<DepartmentForView> getDepartments(){
+        List<DepartmentForView> departmentsForView = new ArrayList<DepartmentForView>();
+
+        // Getting array of Departments form REST API
+        RestTemplate restTemplate = new RestTemplate();
+        String url = applicationURL+"/departments/all";
+        ResponseEntity<Department[]> responseEntity = restTemplate.getForEntity(url, Department[].class);
+        Department[] departments = responseEntity.getBody();
+
+        // Counting average salary for each Department in array
+        boolean hasEmployees;
+        int averageSalary;
+        for(Department dep : departments) {
+            String url2 = applicationURL+"/employees/dep/" + dep.getId();
+            // Trying to get the list of Employees in the Department
+            try {
+                ResponseEntity<Employee[]> responseEntity2 = restTemplate.getForEntity(url2, Employee[].class);
+                Employee[] employees = responseEntity2.getBody();
+                hasEmployees = true;
+                int sum = 0;
+                // Counting average salary of Employees in the Department
+                for (int i = 0; i < employees.length; i++) {
+                    sum += employees[i].getSalary();
+                }
+                averageSalary = sum / employees.length;
+            } catch (Exception e) {
+                hasEmployees = false;
+                averageSalary = 0;
+            }
+            departmentsForView.add(new DepartmentForView(dep, averageSalary, hasEmployees));
+        }
+        return departmentsForView;
+    }
+}
