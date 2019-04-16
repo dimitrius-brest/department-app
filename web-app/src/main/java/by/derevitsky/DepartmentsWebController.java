aa9ca496dc1f051df3fecb3d33854f73066192f0
dimitrius-brest <d_der@mail.ru>
2019-04-16 17:05:27 +0300

    Web module updated

--- web-app/src/main/java/by/derevitsky/DepartmentsWebController.java
+++ web-app/src/main/java/by/derevitsky/DepartmentsWebController.java
@@ -2,7 +2,12 @@ package by.derevitsky;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
@@ -65,5 +70,33 @@ public class DepartmentsWebController {
         ModelAndView model = new ModelAndView("departments");
         model.addObject("departments", departments);
         return model;
+    }
+
+
+    @GetMapping("/add")
+    public String showAddDepartmentForm(Model model){
+        Department department = new Department();
+        model.addAttribute("department", department);
+        return "add_department";
+    }
+
+    /**
+     * Handling the form that adds a new Department
+     * @param department
+     * @param result
+     * @param model
+     * @return
+     */
+    // See:  https://www.baeldung.com/spring-mvc-form-tutorial
+    //       https://www.mkyong.com/spring-mvc/spring-mvc-form-handling-example/
+    @PostMapping("/add")
+    public String addDepartment(@ModelAttribute("department")Department department,
+                                      BindingResult result, ModelMap model){
+        if(result.hasErrors()){
+            return "error";
+        }
+        //model.addAttribute(department.getName());
+
+        return "departments";
     }
 }
---
+++ web-app/src/main/webapp/WEB-INF/jsp/add_department.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Add new Department</title>
+</head>
+<body>
+<h2>Add a new Department:</h2>
+
+<form:form method="post" modelAttribute="department" action="/departments/add">
+    <table>
+        <tr>
+            <td><form:label path="name">Name</form:label></td>
+            <td><form:input path="name"/></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="Add department"/></td>
+        </tr>
+    </table>
+</form:form>
+
+</body>
+</html>
