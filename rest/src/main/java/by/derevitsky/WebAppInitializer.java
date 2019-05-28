93fbadb55d2012688e4d1e67d5d2f9096a2ae02c
dimitrius-brest <d_der@mail.ru>
2019-05-28 16:45:59 +0300

    A test added for REST layer

---
+++ rest/src/main/java/by/derevitsky/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package by.derevitsky;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
--- rest/src/test/java/by/derevitsky/DepartmentRestControllerTest.java
+++ rest/src/test/java/by/derevitsky/DepartmentRestControllerTest.java
@@ -1,10 +1,12 @@
 package by.derevitsky;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,30 +15,37 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.ServletContext;
+
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@WebAppConfiguration
-@ContextConfiguration(locations = "file:rest/src/main/webapp/WEB-INF/department-rest-servlet.xml")
-//@ContextConfiguration(locations = "classpath*:/department-rest-servlet.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebConfig.class})
+//@ContextConfiguration(locations = "file:rest/src/main/webapp/WEB-INF/old-department-rest-servlet.xml")
 public class DepartmentRestControllerTest {
 
-//    @Autowired
-//    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-//    private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-//    @Before
-//    public void setup(){
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-//    }
+    @Before
+    public void setup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void testGetDepartment() throws Exception {
-//        this.mockMvc.perform(get("/{id}"))
-//                .andExpect(status().isOk())
-//                .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON));
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(webApplicationContext.getBean("departmentRestController"));
+
+        this.mockMvc.perform(get("/departments/all"))
+                .andExpect(status().isOk());
+                //.andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON));
     }
 }
