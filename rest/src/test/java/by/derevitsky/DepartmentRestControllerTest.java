0c38423851a80fbb2123ede6d8d4165fd7aea9c3
dimitrius-brest <d_der@mail.ru>
2019-05-30 10:29:05 +0300

    Tests updated for REST layer

--- rest/src/test/java/by/derevitsky/DepartmentRestControllerTest.java
+++ rest/src/test/java/by/derevitsky/DepartmentRestControllerTest.java
@@ -15,7 +15,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -41,18 +46,38 @@ public class DepartmentRestControllerTest {
     @Before
     public void setup(){
         //mockMvc = MockMvcBuilders.standaloneSetup(new DepartmentRestController()).build();
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
     @Test
-    public void testGetAll() throws Exception {
+    public void testContextIsOk() throws Exception {
         ServletContext servletContext = webApplicationContext.getServletContext();
         Assert.assertNotNull(servletContext);
         Assert.assertTrue(servletContext instanceof MockServletContext);
         Assert.assertNotNull(webApplicationContext.getBean("departmentRestController"));
+    }
 
-        this.mockMvc.perform(get("/departments/all"))
-                .andExpect(status().isOk());
+    @Test
+    public void testHello() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/hello");
+        ResultActions result = mockMvc.perform(request);
+        result.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("hello"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("message"));
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+        //this.mockMvc.perform(get("/departments/all"))
+        //        .andExpect(status().isOk());
                 //.andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/departments/all");
+        ResultActions result = mockMvc.perform(request);
+        result.andExpect(status().isOk());
+                //.andDo(MockMvcResultHandlers.print())
+                //.andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON));
+        //result.andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
