ec2de249940b2270178ef39f2bbdda1b3a5f1f86
Dmitry Derevitsky <d_der@mail.ru>
2019-06-05 09:25:27 +0300

    Tests updated for Service classes of Web layer

---
+++ web-app/src/test/java/by/derevitsky/web/EmployeeWebServiceTest.java
@@ -0,0 +1,90 @@
+package by.derevitsky.web;
+
+import by.derevitsky.Employee;
+import by.derevitsky.web.service.EmployeesWebService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmployeeWebServiceTest {
+
+    // See how to mock RestTemplate: https://www.baeldung.com/spring-mock-rest-template
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    EmployeesWebService empWebService = new EmployeesWebService();
+
+    private String applicationURL = "http://localhost:8080/department-rest";
+
+    private Employee[] mockEmployees = new Employee[]{
+            new Employee(1, 1, "Test1",
+                    "Testovich1", "Tester1", LocalDate.parse("1991-01-01"), 500),
+            new Employee(2, 1, "Test2",
+                    "Testovich2", "Tester2", LocalDate.parse("1992-02-02"), 600),
+            new Employee(3, 2, "Test3",
+                    "Testovich3", "Tester3", LocalDate.parse("1993-03-03"), 700)
+    };
+
+    private List<Employee> findInMockEmployeesByDepId(Integer depId){
+        List<Employee> foundEmployees = new ArrayList<Employee>();
+        for(Employee employee : mockEmployees){
+            if(employee.getIdDepartment() == depId) foundEmployees.add(employee);
+        }
+        return foundEmployees;
+    }
+
+    @Test
+    public void testGetEmployeeById() throws Exception {
+        Employee mockEmployee = new Employee(1, 1, "Test",
+                "Testovich", "Tester", LocalDate.parse("1999-09-09"), 1000);
+        Mockito
+                .when(restTemplate.getForEntity(applicationURL+"/employees/1", Employee.class))
+                .thenReturn(new ResponseEntity(mockEmployee, HttpStatus.OK));
+        Employee employee = empWebService.getEmployeeById(1);
+        Assert.assertEquals(mockEmployee, employee);
+    }
+
+    @Test
+    public void testGetAllEmployees() throws Exception {
+        Mockito
+                .when(restTemplate.getForEntity(applicationURL+"/employees/all", Employee[].class))
+                .thenReturn(new ResponseEntity(mockEmployees, HttpStatus.OK));
+        int totalNumberOfEmployees = empWebService.getAllEmployees().size();
+        Assert.assertEquals(3, totalNumberOfEmployees);
+    }
+
+    @Test
+    public void testGetEmployeesByDepartmentId() throws Exception{
+        Employee[] employeesInDep1 = findInMockEmployeesByDepId(1).toArray(
+                new Employee[findInMockEmployeesByDepId(1).size()]);
+
+        Employee[] employeesInDep2 = findInMockEmployeesByDepId(2).toArray(
+                new Employee[findInMockEmployeesByDepId(2).size()]);
+
+        Mockito
+                .when(restTemplate.getForEntity(applicationURL+"/employees/dep/1", Employee[].class))
+                .thenReturn(new ResponseEntity(employeesInDep1, HttpStatus.OK));
+        Mockito
+                .when(restTemplate.getForEntity(applicationURL+"/employees/dep/2", Employee[].class))
+                .thenReturn(new ResponseEntity(employeesInDep2, HttpStatus.OK));
+
+        // Nuber of Employees in Department #1
+        Assert.assertEquals(2, empWebService.getEmployeesByDepartmentId(1).size());
+        // Nuber of Employees in Department #2
+        Assert.assertEquals(1, empWebService.getEmployeesByDepartmentId(2).size());
+    }
+}
