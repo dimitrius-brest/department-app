7332ac0151e9259aa58fbd903cb1118f700b42da
dimitrius-brest <d_der@mail.ru>
2020-01-30 12:28:44 +0300

    Some experiments with logging

--- rest/src/main/java/by/derevitsky/EmployeeRestController.java
+++ rest/src/main/java/by/derevitsky/EmployeeRestController.java
@@ -1,6 +1,8 @@
 package by.derevitsky;
 
 import org.apache.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ public class EmployeeRestController {
 
     // Logging
     private static final Logger logger = Logger.getLogger(EmployeeRestController.class);
+    //private static final Logger logger = LogManager.getLogger(EmployeeRestController.class);
 
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Employee> getEmployee(@PathVariable("id") Integer id){
