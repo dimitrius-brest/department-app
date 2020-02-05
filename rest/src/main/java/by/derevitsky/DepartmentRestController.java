477d74d5d6ea153f4b13d5d4efc2af7e6d803f0a
dimitrius-brest <d_der@mail.ru>
2020-02-05 15:46:10 +0300

    small fix

--- rest/src/main/java/by/derevitsky/DepartmentRestController.java
+++ rest/src/main/java/by/derevitsky/DepartmentRestController.java
@@ -4,7 +4,6 @@ import by.derevitsky.model.Department;
 import org.apache.log4j.Logger;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
-import org.omg.CORBA.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
