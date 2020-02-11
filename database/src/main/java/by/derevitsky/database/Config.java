90fe0702146f90f2f08fbac04dca5e1b40e6d671
dimitrius-brest <d_der@mail.ru>
2020-02-11 17:29:15 +0300

    New layer "database" added

---
+++ database/src/main/java/by/derevitsky/database/Config.java
@@ -0,0 +1,10 @@
+package by.derevitsky.database;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+@ComponentScan(basePackages = "by.derevitsky.database")
+public class Config {
+
+}
