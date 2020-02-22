f97d03d7b8d93567df31c7fc626c831894c7a875
Dmitry Derevitsky <d_der@mail.ru>
2020-02-22 13:01:37 +0300

    Some tests migrated from junit 4 to junit 5

--- dao-jdbc/src/test/java/by/derevitsky/dao/DepartmentDaoCrudTest.java
+++ dao-jdbc/src/test/java/by/derevitsky/dao/DepartmentDaoCrudTest.java
@@ -2,24 +2,37 @@ package by.derevitsky.dao;
 
 import by.derevitsky.dao.DepartmentDAO;
 import by.derevitsky.model.Department;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
+
+// --------- Junit 4 ---------
+//import org.junit.Assert;
+//import org.junit.Ignore;
+//import org.junit.Test;
+// import org.junit.FixMethodOrder;
+// import org.junit.runner.RunWith;
+// import org.junit.runners.MethodSorters;
+
+// --------- Junit 5 ---------
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.springframework.test.context.junit.jupiter.SpringExtension;          // for junit 5
+// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;      // for junit 4
 import java.util.List;
 
+//@RunWith(SpringJUnit4ClassRunner.class)       // junit 4
+@ExtendWith(SpringExtension.class)              // junit 5
 @ContextConfiguration("classpath:dao_context.xml")
-@RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(profiles = {"jdbc", "h2mem"})
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)     // junit 4
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)  // junit 5
 public class DepartmentDaoCrudTest {
 
     @Autowired @Qualifier("JdbcDepartmentDAO")
@@ -28,17 +41,19 @@ public class DepartmentDaoCrudTest {
     @Test
     public void test1_GetAll() throws Exception {
         List<Department> departments = departmentDAO.getAll();
-        Assert.assertNotNull(departments);                      // The list of Departments is not empty
-        Assert.assertEquals(4, departments.size());     // The list of Departments contains 4 Departments
+        //Assert.assertNotNull(departments);                      // The list of Departments is not empty
+        assertNotNull(departments);
+        /*Assert.*/assertEquals(4, departments.size());     // The list of Departments contains 4 Departments
     }
 
-    @Ignore
+    //@Ignore       // junit 4
+    //@Disabled     // junit 5
     @Test
     public void test2_GetById() throws Exception {
-        Assert.assertEquals("CEO", departmentDAO.getById(1).getName());           // Department id=1
-        Assert.assertEquals("Economists", departmentDAO.getById(2).getName());    // Department id=2
-        Assert.assertEquals("Programmers", departmentDAO.getById(3).getName());   // Department id=3
-        Assert.assertEquals("Dummies", departmentDAO.getById(4).getName());       // Department id=4
+        assertEquals("CEO-Mem", departmentDAO.getById(1).getName());           // Department id=1
+        assertEquals("Economists-Mem", departmentDAO.getById(2).getName());    // Department id=2
+        assertEquals("Programmers-Mem", departmentDAO.getById(3).getName());   // Department id=3
+        assertEquals("Dummies-Mem", departmentDAO.getById(4).getName());       // Department id=4
     }
 
     @Test
@@ -46,7 +61,7 @@ public class DepartmentDaoCrudTest {
         departmentDAO.insert(new Department(0, "Test Department"));
         List<Department> departments = departmentDAO.getAll();
         Department lastDepartment = departments.get(departments.size() - 1);     // Last Department after inserting
-        Assert.assertEquals("Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
+        assertEquals("Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
     }
 
     @Test
@@ -55,21 +70,22 @@ public class DepartmentDaoCrudTest {
         Department lastDepartment = departments.get(departments.size() - 1);
         lastDepartment.setName("Updated Test Department");
         departmentDAO.update(lastDepartment);                                    // Updating last Department
-        Assert.assertEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
+        assertEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
     }
 
-    @Ignore
+    //@Ignore       // junit 4
+    //@Disabled     // junit 5
     @Test
     public void test5_Delete() throws Exception {
         List<Department> departments = departmentDAO.getAll();
-        Assert.assertEquals(5, departments.size());     // The updated list of Departments contains 5 Departments
+        assertEquals(5, departments.size());     // The updated list of Departments contains 5 Departments
         Department lastDepartment = departments.get(departments.size() - 1);
-        Assert.assertEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
+        assertEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
         departmentDAO.delete(lastDepartment.getId());           // Deleting last Department
         departments = departmentDAO.getAll();
-        Assert.assertEquals(4, departments.size());     // Now the list of Departments contains 4 Departments
+        assertEquals(4, departments.size());     // Now the list of Departments contains 4 Departments
         lastDepartment = departments.get(departments.size() - 1);
-        Assert.assertNotEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
-        Assert.assertEquals("Dummies", departmentDAO.getById(lastDepartment.getId()).getName());
+        assertNotEquals("Updated Test Department", departmentDAO.getById(lastDepartment.getId()).getName());
+        assertEquals("Dummies-Mem", departmentDAO.getById(lastDepartment.getId()).getName());
     }
 }
--- dao-jdbc/src/test/java/by/derevitsky/dao/EmployeeDaoCrudTest.java
+++ dao-jdbc/src/test/java/by/derevitsky/dao/EmployeeDaoCrudTest.java
@@ -2,25 +2,38 @@ package by.derevitsky.dao;
 
 import by.derevitsky.dao.EmployeeDAO;
 import by.derevitsky.model.Employee;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
+
+// --------- Junit 4 ---------
+//import org.junit.Assert;
+//import org.junit.FixMethodOrder;
+//import org.junit.Ignore;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.MethodSorters;
+
+// --------- Junit 5 ---------
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;          // for junit 5
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;       // for junit 4
 
 import java.time.LocalDate;
 import java.util.List;
 
+//@RunWith(SpringJUnit4ClassRunner.class)       // junit 4
+@ExtendWith(SpringExtension.class)              // junit 5
 @ContextConfiguration("classpath:dao_context.xml")
-@RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(profiles = {"jdbc", "h2mem"})
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)     // junit 4
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)  // junit 5
 public class EmployeeDaoCrudTest {
 
     @Autowired @Qualifier("JdbcEmployeeDAO")
@@ -29,29 +42,29 @@ public class EmployeeDaoCrudTest {
     @Test
     public void test1_GetAll() throws Exception {
         List<Employee> employees = employeeDAO.getAll();
-        Assert.assertNotNull(employees);                    // The list of Employees is not empty
-        Assert.assertEquals(5, employees.size());   // The list of Employees contains 4 Departments
+        assertNotNull(employees);                    // The list of Employees is not empty
+        assertEquals(5, employees.size());   // The list of Employees contains 4 Departments
     }
 
     @Test
     public void test2_GetById() throws Exception {
         Employee employee = employeeDAO.getById(1);
-        Assert.assertEquals(1, employee.getIdDepartment());
-        Assert.assertEquals("Ivan", employee.getFirstName());
-        Assert.assertEquals("Ivanovich", employee.getMiddleName());
-        Assert.assertEquals("Ivanov", employee.getLastName());
-        Assert.assertEquals(LocalDate.parse("1970-01-01"), employee.getBirthDate());
-        Assert.assertEquals(10000, employee.getSalary());
+        assertEquals(1, employee.getIdDepartment());
+        assertEquals("Ivan", employee.getFirstName());
+        assertEquals("Ivanovich", employee.getMiddleName());
+        assertEquals("Ivanov", employee.getLastName());
+        assertEquals(LocalDate.parse("1970-01-01"), employee.getBirthDate());
+        assertEquals(10000, employee.getSalary());
     }
 
     @Test
     public void test3_GetByDepartmentId() throws Exception {
         List<Employee> employeesInDepartment = employeeDAO.getByDepartmentId(1); // Employees in Department with id=1
-        Assert.assertEquals(1, employeesInDepartment.size());
+        assertEquals(1, employeesInDepartment.size());
         employeesInDepartment = employeeDAO.getByDepartmentId(2);               // Employees in Department with id=2
-        Assert.assertEquals(2, employeesInDepartment.size());
+        assertEquals(2, employeesInDepartment.size());
         employeesInDepartment = employeeDAO.getByDepartmentId(4);               // Employees in Department with id=1
-        Assert.assertEquals(0, employeesInDepartment.size());
+        assertEquals(0, employeesInDepartment.size());
     }
 
     @Test
@@ -61,12 +74,12 @@ public class EmployeeDaoCrudTest {
         List<Employee> employees = employeeDAO.getAll();
         Employee lastEmployee = employees.get(employees.size() - 1);   // Last Employee after inserting
         int lastId = lastEmployee.getId();
-        Assert.assertEquals(4, employeeDAO.getById(lastId).getIdDepartment());
-        Assert.assertEquals("Test", employeeDAO.getById(lastId).getFirstName());
-        Assert.assertEquals("Testovich", employeeDAO.getById(lastId).getMiddleName());
-        Assert.assertEquals("Tester", employeeDAO.getById(lastId).getLastName());
-        Assert.assertEquals(LocalDate.parse("1900-01-01"), employeeDAO.getById(lastId).getBirthDate());
-        Assert.assertEquals(500, employeeDAO.getById(lastId).getSalary());
+        assertEquals(4, employeeDAO.getById(lastId).getIdDepartment());
+        assertEquals("Test", employeeDAO.getById(lastId).getFirstName());
+        assertEquals("Testovich", employeeDAO.getById(lastId).getMiddleName());
+        assertEquals("Tester", employeeDAO.getById(lastId).getLastName());
+        assertEquals(LocalDate.parse("1900-01-01"), employeeDAO.getById(lastId).getBirthDate());
+        assertEquals(500, employeeDAO.getById(lastId).getSalary());
     }
 
     @Test
@@ -78,21 +91,21 @@ public class EmployeeDaoCrudTest {
                 "UTest", "UTestovich", "Updated-Tester",
                 LocalDate.parse("2000-02-02"), 3000);
         employeeDAO.update(updatedEmployee);                         //  Doing update
-        Assert.assertEquals(1, employeeDAO.getById(lastId).getIdDepartment());
-        Assert.assertEquals("UTest", employeeDAO.getById(lastId).getFirstName());
-        Assert.assertEquals("UTestovich", employeeDAO.getById(lastId).getMiddleName());
-        Assert.assertEquals("Updated-Tester", employeeDAO.getById(lastId).getLastName());
-        Assert.assertEquals(LocalDate.parse("2000-02-02"), employeeDAO.getById(lastId).getBirthDate());
-        Assert.assertEquals(3000, employeeDAO.getById(lastId).getSalary());
+        assertEquals(1, employeeDAO.getById(lastId).getIdDepartment());
+        assertEquals("UTest", employeeDAO.getById(lastId).getFirstName());
+        assertEquals("UTestovich", employeeDAO.getById(lastId).getMiddleName());
+        assertEquals("Updated-Tester", employeeDAO.getById(lastId).getLastName());
+        assertEquals(LocalDate.parse("2000-02-02"), employeeDAO.getById(lastId).getBirthDate());
+        assertEquals(3000, employeeDAO.getById(lastId).getSalary());
     }
 
     @Test
     public void test6_Delete() throws Exception {
         List<Employee> employees = employeeDAO.getAll();
         Employee lastEmployee = employees.get(employees.size() - 1);
-        Assert.assertEquals(6, employees.size());       // Number of Employees should be 6
+        assertEquals(6, employees.size());       // Number of Employees should be 6
         employeeDAO.delete(lastEmployee.getId());               // Deleting last Employee
         employees = employeeDAO.getAll();
-        Assert.assertEquals(5, employees.size());       // Number of Employees should be 5
+        assertEquals(5, employees.size());       // Number of Employees should be 5
     }
 }
