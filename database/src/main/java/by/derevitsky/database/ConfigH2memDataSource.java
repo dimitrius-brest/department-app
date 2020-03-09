810c5033089be21fe050f3c5e6b8e579a07bf07a
dimitrius-brest <d_der@mail.ru>
2020-03-09 16:05:53 +0300

    Settings of H2-server database moved from "database-h2-server" to "database" module

--- database/src/main/java/by/derevitsky/database/ConfigH2memDataSource.java
+++ database/src/main/java/by/derevitsky/database/ConfigH2memDataSource.java
@@ -1,9 +1,10 @@
 package by.derevitsky.database;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -13,6 +14,8 @@ import java.util.Properties;
 @Configuration
 @ComponentScan(basePackages = "by.derevitsky")
 public class ConfigH2memDataSource {
+
+    // --------------------------- @Profile  h2mem --------------------------
     /**
      * Sets DataSource of H2 in-memory database
      * @return new in-memory database instance, populated with test data from *.sql files
@@ -37,5 +40,21 @@ public class ConfigH2memDataSource {
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         return properties;
+    }
+
+    // --------------------------- @Profile  h2server --------------------------
+    /**
+     * Sets DataSource of H2 server database
+     * @return DataSource
+     */
+    @Profile("h2server")
+    @Bean(name="H2serverDataSource")
+    public DataSource setDataSourceH2server(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 }
