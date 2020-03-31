8763f572e284f19e5ccd362a73dd3e8b477087db
dimitrius-brest <d_der@mail.ru>
2020-03-31 15:26:32 +0300

    All CRUD methods for "Departments" in "soap" layer updated

--- soap/src/main/java/by/derevitsky/soap/DepartmentSoap.java
+++ soap/src/main/java/by/derevitsky/soap/DepartmentSoap.java
@@ -5,24 +5,23 @@ import by.derevitsky.model.Department;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import java.util.List;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface DepartmentSoap {
 
-//    @WebMethod
-//    List<Department> getAllDepartments();
-//
+    @WebMethod
+    Department[] getAllDepartments();
+
     @WebMethod
     Department getDepartmentById(Integer id);
-//
-//    @WebMethod
-//    void insertDepartment(Department department);
-//
-//    @WebMethod
-//    void updateDepartment(Department department);
-//
-//    @WebMethod
-//    void deleteDepartment(Integer id);
+
+    @WebMethod
+    void insertDepartment(Department department);
+
+    @WebMethod
+    void updateDepartment(Department department);
+
+    @WebMethod
+    void deleteDepartment(Integer id);
 }
--- soap/src/main/java/by/derevitsky/soap/HelloWorldClient.java
+++ soap/src/main/java/by/derevitsky/soap/HelloWorldClient.java
@@ -5,6 +5,8 @@ import by.derevitsky.model.Department;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloWorldClient {
     public static void main(String[] args) throws Exception {
@@ -24,7 +26,35 @@ public class HelloWorldClient {
         //HelloWorld hello = service.getPort(HelloWorld.class);
         DepartmentSoap depSoap = service2.getPort(DepartmentSoap.class);
 
-        Department department = depSoap.getDepartmentById(33);
+        System.out.println("------ Get by id: -------");
+        Department department = depSoap.getDepartmentById(2);
         System.out.println("ID = " + department.getId() + "; name = " + department.getName());
+
+        System.out.println("------ Get all: -------");
+        List<Department> departments = Arrays.asList(depSoap.getAllDepartments());
+        for (Department dep: departments) {
+            System.out.println("ID: " + dep.getId() + "; name: " + dep.getName());
+        }
+
+        System.out.println("----- Inserting new department -----");
+        depSoap.insertDepartment(new Department(3, "Stub Dep. 3"));
+        departments = Arrays.asList(depSoap.getAllDepartments());
+        for (Department dep: departments) {
+            System.out.println("ID: " + dep.getId() + "; name: " + dep.getName());
+        }
+
+        System.out.println("----- Updating a department -----");
+        depSoap.updateDepartment(new Department(2, "Updated Stub Dep. 222"));
+        departments = Arrays.asList(depSoap.getAllDepartments());
+        for (Department dep: departments) {
+            System.out.println("ID: " + dep.getId() + "; name: " + dep.getName());
+        }
+
+        System.out.println("----- Deleting a department -----");
+        depSoap.deleteDepartment(2);
+        departments = Arrays.asList(depSoap.getAllDepartments());
+        for (Department dep: departments) {
+            System.out.println("ID: " + dep.getId() + "; name: " + dep.getName());
+        }
     }
 }
